097198e338480d017fa8fab7c573347eb299d863
Angeline Rao <angierao@fb.com>
2016-06-28 15:02:36 -0700

    pre meeting commit

---
+++ twitter/ProfileCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileCell.swift
+//  twitter
+//
+//  Created by Angeline Rao on 6/28/16.
+//  Copyright © 2016 Angeline Rao. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ twitter/ProfileViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProfileViewController.swift
+//  twitter
+//
+//  Created by Angeline Rao on 6/28/16.
+//  Copyright © 2016 Angeline Rao. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
