80dccf0d3a8e6b145d374a7e5fee7e30882d3bde
Angeline Rao <angierao@fb.com>
2016-06-29 11:55:28 -0700

    clicking a prof pic leads to their profile

--- twitter/UserViewController.swift
+++ twitter/UserViewController.swift
@@ -8,14 +8,55 @@
 
 import UIKit
 
-class UserViewController: UIViewController {
+class UserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var tweets: [Tweet]?
     var user: User?
+    
+    @IBOutlet weak var profPicView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var twitterNameLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        TwitterClientSM.sharedInstance.profileTimeline((user!.screenname)! as String, success: { (tweets: [Tweet]) in
+            self.tweets = tweets
+            self.tableView.reloadData()
+            
+            }, failure: { (error: NSError) in
+                print(error)
+        })
+        
+        nameLabel.text = user!.name as? String
+        let twitterName = user!.screenname as! String
+        twitterNameLabel.text = "@\(twitterName)"
+        let imageRequest = NSURLRequest(URL: user!.profileUrl!)
+        
+        profPicView.setImageWithURLRequest(imageRequest, placeholderImage: UIImage(named: "defaulttwitter"), success: { (request: NSURLRequest, response: NSHTTPURLResponse?, image: UIImage) in
+            self.profPicView.image = image
+        }) { (request: NSURLRequest, response: NSHTTPURLResponse?, error: NSError) in
+            print(error)
+        }
+
+
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets?.count ?? 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let userTweetCell = tableView.dequeueReusableCellWithIdentifier("UserTweetCell") as! UserTweetCell
+        userTweetCell.tweet = tweets![indexPath.row]
+        return userTweetCell
     }
 
     override func didReceiveMemoryWarning() {
